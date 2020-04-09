5afb4966f802e5d108fad1b7a471e8f5559eb397
jaamunozr <jaamunozr@gmail.com>
2020-04-09 06:02:33 -0500

    prueba

--- Conversor_A-D/pruebadatostiempo.py
+++ Conversor_A-D/pruebadatostiempo.py
@@ -2,17 +2,28 @@ import time
 import Adafruit_ADS1x15
 import datetime
 adc = Adafruit_ADS1x15.ADS1115()
-GAIN = 1
+GAIN = 2/3
 
 print('Reading ADS1x15 values, press Ctrl-C to quit...')
 print('| {0:>6} '.format(1))
 print('-' * 37)
 k=0
-while True:
-    k=k+1
-    #values= adc.read_adc(0, gain=GAIN)
-    values= adc.read_adc(0, gain=GAIN, data_rate=860)
-    #print('| {0:>6} | '.format(values))    
-    #time.sleep(0.5)
-    print(k)
-    print(datetime.datetime.now())
+tension=[]
+tiempo=[]
+p=1
+while p==1:
+    while k<1000:
+        k=k+1
+        #time.sleep(0.5)
+        tension.append(adc.read_adc(0, gain=GAIN, data_rate=860))
+        #print(values)
+        tiempo.append(datetime.datetime.now())
+    else:
+        tension=str(tension)
+        tiempo=str(tiempo)
+        texto=open("texto.xlsx","w")
+        texto.write(tension + '\n')
+        #texto.write("
+        texto.write(tiempo)
+        texto.close()
+    p=0
